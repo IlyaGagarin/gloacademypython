c7ca58ef0abb86672d75e73b0a3a3eaacd2c18d7
IlyaGagarin <85702065+IlyaGagarin@users.noreply.github.com>
2021-06-14 21:21:05 +0300

    Add files via upload

---
+++ урок4/zadanie1.py
@@ -0,0 +1,4 @@
+# -- coding: utf-8 
+x=int(input())
+y=int(input())
+print(x**2+y**2)
---
+++ урок4/zadanie4.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 
+r=int(input())
+k=int(input())
+n=int(input())
+print ('За ', n, 'мяча нужно заплатить', n*r,'рублей и ', k*n, 'копеек')
