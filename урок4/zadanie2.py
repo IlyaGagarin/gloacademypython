71915e53092a160d85e6392e396102c4203f8114
IlyaGagarin <85702065+IlyaGagarin@users.noreply.github.com>
2021-06-14 21:38:19 +0300

    Add files via upload

---
+++ урок4/zadanie2.py
@@ -0,0 +1,8 @@
+# -- coding: utf-8 
+x=int(input())
+y=int(input())
+print (x, 'умноженное на ', y, 'равно ', x*y, end='\n\n')
+print (x, 'деленное на ', y, 'равно ', float(x/y), end='\n\n')
+print (x, 'нацело деленное на ', y, 'равно ', x//y, end='\n\n')
+print ('Остаток от деления ', x, 'на ', y, 'равен ', x%y, end='\n\n')
+print (x, 'в степени ', y, 'равно ', x**y, end='\n\n')
