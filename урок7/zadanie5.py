b8671ece6256c6f1a91b60c8c243aca4a826a777
IlyaGagarin <85702065+IlyaGagarin@users.noreply.github.com>
2021-06-18 19:44:23 +0300

    Delete zadanie5.py

--- урок7/zadanie5.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 
-number=int(input())
-total=0
-for i in range (number):
-    if (number % 10) == 1 or number % 10 == 3 or number % 10 == 7:
-        total = total + number
-print (total)
-        
